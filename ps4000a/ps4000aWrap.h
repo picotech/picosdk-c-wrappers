53f1b98672eeafa89f3af46b0c10e89fc28befcd
hitesh.mistry <hitesh.mistry@picotech.com>
2017-04-07 18:47:34 +0100

    Added setPulseWidthQualifierConditions function

--- ps4000a/ps4000aWrap.h
+++ ps4000a/ps4000aWrap.h
@@ -179,6 +179,12 @@ extern PICO_STATUS PREF0 PREF1 setTriggerProperties
 	int32_t autoTrig
 );
 
-
+extern PICO_STATUS PREF0 PREF1 setPulseWidthQualifierConditions
+(
+	int16_t handle,
+	int32_t *conditionsArray,
+	int16_t nConditions,
+	int32_t info
+);
 
 #endif
